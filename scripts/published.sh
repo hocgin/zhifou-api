1bbc1ff8bddc5826d0145157a318240ebfa70e0b
hocgin <hocgin@gmail.com>
2019-05-06 15:18:33 +0800

    update

--- scripts/published.sh
+++ scripts/published.sh
@@ -1,10 +1,10 @@
 # 发布文章脚本
 #!/usr/bin/env bash
 ip=130.211.243.95
-path=/prod/post
+path=/prod
 
 # ========================
 ssh root@$ip "rm -rf $path/*"
-scp -r ./docs/post/* root@$ip:$path/
+scp -r ./docs/post root@$ip:$path/
 echo -e '\n'
 date
---
+++ src/main/java/in/hocg/share/app/support/CustomFilter.java
@@ -0,0 +1,49 @@
+package in.hocg.share.app.support;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by hocgin on 2018/12/30.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@WebFilter(filterName = "CustomFilter", urlPatterns = "/**")
+public class CustomFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+        log.debug("开启自定义拦截器");
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletResponse servletResponse = (HttpServletResponse) response;
+        HttpServletRequest servletRequest = (HttpServletRequest) request;
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("application/json; charset=utf-8");
+        String origin = servletRequest.getHeader("Origin");
+        servletResponse.setHeader("Access-Control-Allow-Origin", origin);
+        servletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        servletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        servletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        servletResponse.setHeader("Access-Control-Expose-Headers", "*");
+        chain.doFilter(request, servletResponse);
+    }
+    
+    @Override
+    public void destroy() {
+    }
+}
